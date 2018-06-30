86d55b6eb86f15f92f8f1b7731d2f8b22c173cdb
Zeng Mac <zengmac@Zeng.local>
2018-07-01 01:26:27 +0800

    feature/userRegister

--- controller/user.js
+++ controller/user.js
@@ -1,3 +1,25 @@
-function registerUser(username, email, password) {
-    
+import { regExp_validate } from './validate'
+
+export function registerUser(username, email, password) {
+    if (!regExp_validate.usernameReg.test(username) && !regExp_validate.lowpw.test(password) && !regExp_validate.email.test(email)) return false;
+    var connection = mysql.createConnection(sqlconfig); 
+    connection.connect();
+    connection.query('insert into project.user value(?, ?, ?)', [username, email, password],  function (error, results, fields) {
+        if (error) throw error;
+    });
+    connection.end();
+}
+
+export function userexisted(email) {
+    var connection = mysql.createConnection(sqlconfig); 
+    connection.connect();
+
+    const [ res ] = await (new Promise((resolve,reject) =>{
+	    connection.query('select * from project.user where email = ?' , [email], function (error, results, fields) {
+	        if (error) throw error;
+	        resolve(results);
+	    });
+    }))
+    connection.end();
+    return !!res;
 }
