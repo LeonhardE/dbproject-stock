b568960ccaa1fd8566511ecc219ffe1c30937340
Zeng Mac <zengmac@Zeng.lan>
2018-07-02 13:56:39 +0800

    bugfix/router change

--- controller/user.js
+++ controller/user.js
@@ -2,19 +2,32 @@ import mysql from 'mysql';
 import sqlconfig from '../config'
 import { regExp_validate } from './validate';
 import { passwdSha, compareSha } from './passwd-user';
+import bluebird from "bluebird"
 
-export async function registerUser(username, email, password) {
-    if (!regExp_validate.usernameReg.test(username) && !regExp_validate.lowpw.test(password) && !regExp_validate.email.test(email)) return false;
+export async function registerUser(ctx, next) {
+    const { username : username, email : email, passwd : passwd} = ctx.request.body;
+    if (!regExp_validate.usernameReg.test(username) && !regExp_validate.lowpw.test(passwd) && !regExp_validate.email.test(email)) return false;
     const existed = await userexisted(email);
     if (!existed) return false;
     var connection = mysql.createConnection(sqlconfig); 
-    const pwd = passwdSha(password);
+    const pwd = passwdSha(passwd);
     connection.connect();
-    connection.query('insert into project.user value(?, ?, ?)', [username, email, pwd],  function (error, results, fields) {
-        if (error) throw error;
+    return await new Promise((resolve, reject)=>{
+        connection.query('insert into project.user value(?, ?, ?)', [username, email, pwd],  function (error, results, fields) {
+            if (error) { 
+                ctx.body = {
+                    msg : error,
+                    status: false,
+                };
+                reject(error);
+            }
+            connection.end();
+            ctx.body = {
+                status: true
+            };
+            resolve();
+        });
     });
-    connection.end();
-    return true;
 }
 
 export async function userexisted(email) {
@@ -31,7 +44,8 @@ export async function userexisted(email) {
     return res.length == 0;
 }
 
-export async function userlogin(email, passwd) {
+export async function userlogin(ctx, next) {
+    const { email : email, passwd : passwd} = ctx.request.body;
     var connection = mysql.createConnection(sqlconfig);
     connection.connect();
     const [ res ] = await (new Promise((resolve, reject) =>{
@@ -41,7 +55,9 @@ export async function userlogin(email, passwd) {
 	    });
     }))
     connection.end();
-    if (res && compareSha(passwd, res["password"])) return true;
+    if (res && compareSha(passwd, res["password"])) {
+        
+    };
     return false;
 }
 
--- routes/user.js
+++ routes/user.js
@@ -3,15 +3,9 @@ import { registerUser, userlogin, StockofUser } from "../controller/user"
 
 const router = Router()
 
-router.post('/register', async (ctx, next) => {
-    const { username : username, email : email, passwd : passwd} = ctx.request.body;
-    ctx.body = await registerUser(username, email, passwd);
-});
+router.post('/register', registerUser);
 
-router.post('/login', async (ctx, next) => {
-    const { email : email, passwd : passwd} = ctx.request.body;
-    ctx.body = await userlogin(email, passwd);
-});
+router.post('/login', userlogin);
 
 router.get('/prefer', async (ctx, next) => {
     const { email : email } = ctx.request.query;
